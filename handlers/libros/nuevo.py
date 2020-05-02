e0864332eded8623b36c12ec19197354da0a2d59
jsevilla <jsevilla@aldaba.es>
2020-05-02 12:43:24 +0200

    añadir libro

--- handlers/libros/nuevo.py
+++ handlers/libros/nuevo.py
@@ -4,6 +4,7 @@
 import webapp2
 from webapp2_extras import jinja2
 from model.libro import Libro
+import time
 
 class NuevoLibroHandler(webapp2.RequestHandler):
     def get(self):
@@ -16,7 +17,22 @@ class NuevoLibroHandler(webapp2.RequestHandler):
         )
 
     def post(self):
-        self.response.write("Creación de libro")
+        titulo = self.request.get("titulo")
+        autor = self.request.get("autor")
+        str_anho = self.request.get("anho")
+        enlace = self.request.get("enlace")
+        try:
+            anho = int(str_anho)
+        except ValueError:
+            anho = -1
+        if (anho < 0 or not(titulo) or not (autor) or not (enlace)) :
+            return self.response.write(str_anho)
+        else:
+            libro = Libro(titulo=titulo, autor=autor, anho=anho, enlace=enlace)
+            libro.put()
+            time.sleep(1)
+            return self.redirect("/")
+        #self.response.write("Creación de libro")
 
 app = webapp2.WSGIApplication([
     ('/libros/nuevo', NuevoLibroHandler)
